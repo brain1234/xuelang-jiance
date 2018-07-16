2457ddebf6afc37a62c579fbd60942b0cc6274bd
hzj <huzhongj@163.com>
2018-07-16 17:46:26 +0800

    commit

--- utils/getImages.py
+++ utils/getImages.py
@@ -8,15 +8,22 @@ def get_nor_imgs(dir):
     :param dir: 文件夹的路径
     :return: 获取正常无瑕疵的图片
     """
-    if os._exists(dir):
-        for f in os.listdir(dir):
-            print(f)
+    abs_path = os.path.abspath(dir)
+    filenames = os.listdir(dir)
+    print(abs_path)
+    filenames = [os.path.join(abs_path, i) for i in filenames]
+    print(filenames)
+    for file in filenames:
+        if os.path.isfile(file):
+            print("是个文件，不是目录")
+
 
 def get_flaw_imgs(dir):
     """
     :param dir: 文件夹路径
     :return: 获取有瑕疵的图片
     """
+
 
 if __name__ == "__main__":
     get_nor_imgs(r"../models")
