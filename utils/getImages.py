d7cb62e0fe6a27a241eee70e68ec2b96c83b70d0
brain1234 <huzhongj@163.com>
2018-07-25 03:21:40 +0800

    splice train valication data set

--- utils/getImages.py
+++ utils/getImages.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import random
 import shutil
 
 
@@ -19,10 +20,41 @@ def get_imgs(dir):
             print(os.path.join(abs_path, fi))
             if os.path.splitext(fi)[1] != ".xml":
                 if os.path.dirname(fi).endswith("正常"):
-                    shutil.copyfile(os.path.join(abs_path, fi), "../data/train/normal/" + os.path.basename(fi))
+                    shutil.copyfile(os.path.join(abs_path, fi), "../data/test/normal/" + os.path.basename(fi))
                 else:
-                    shutil.copyfile(os.path.join(abs_path, fi), "../data/train/flaw/" + os.path.basename(fi))
+                    shutil.copyfile(os.path.join(abs_path, fi), "../data/test/flaw/" + os.path.basename(fi))
+
+
+def get_train_valication_imgs(dir, des_train_dir, des_valication_dir):
+    """
+    获取训练集 80%, 验证集 20%
+    """
+    fileName = []
+    abs_path = os.path.abspath(dir)
+    fileNames = os.listdir(dir)
+    print(os.pardir)
+    for f in fileNames:
+        if not os.path.isdir(os.path.join(abs_path, f)):
+            fileName.append(f)
+    # shuffle
+    random.shuffle(fileName)
+    # sample
+    valication = random.sample(fileName, int(len(fileName) * 0.2))
+    train = list(set(fileName).difference(set(valication)))
+    print("ori size", len(fileName))
+    print("train size", len(train))
+    print("valication size", len(valication))
+
+    for f1 in train:
+        shutil.copy(os.path.join(dir, f1), os.path.join(des_train_dir, f1))
+
+    for f1 in valication:
+        shutil.copy(os.path.join(dir, f1), os.path.join(des_valication_dir, f1))
 
 
 if __name__ == "__main__":
-    get_imgs(r"F:\天池比赛项目\雪浪制造AI挑战赛\data\xuelang_round1_train_part3_20180709")
+    # get_imgs(r"F:\天池比赛项目\雪浪制造AI挑战赛\data\xuelang_round1_train_part1_20180628")
+    # get_imgs(r"F:\天池比赛项目\雪浪制造AI挑战赛\data\xuelang_round1_train_part2_20180705")
+    # get_imgs(r"F:\天池比赛项目\雪浪制造AI挑战赛\data\xuelang_round1_train_part3_20180709")
+    get_train_valication_imgs("../data/test/flaw", "../data/train/flaw", "../data/valication/flaw")
+    get_train_valication_imgs("../data/test/normal", "../data/train/normal", "../data/valication/normal")
